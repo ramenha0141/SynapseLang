ec9830878eac0071753738b1cf79643be61910cc
ramenha0141 <ramenhaoishii@gmail.com>
2022-05-12 20:10:20 +0900

    update

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -2,15 +2,3 @@ import { expectSingleResult, expectEOF } from 'typescript-parsec';
 import lexer from './lexer.js';
 import parser from './parser.js';
 export default (string: string) => expectSingleResult(expectEOF(parser.parse(lexer.parse(string))));
-const string = `import console;
-import Math;
-import process;
-class A {
-    constructor() {}
-    str: char[1];
-    length: int;
-}
-function main(): void {
-    const a: A = new A();
-}`;
-console.log(JSON.stringify(expectSingleResult(expectEOF(parser.parse(lexer.parse(string))))));
