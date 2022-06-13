2e6b43404f5e98712444a9fefc4f6b0546c134cd
ramenha0141 <ramenhaoishii@gmail.com>
2022-06-13 18:55:32 +0900

    update

--- src/synac.ts
+++ src/synac.ts
@@ -1,5 +1,5 @@
 import compile from './compile.js';
 import parse_parameter from './parse_parameter.js';
 
-const options = parse_parameter(process.argv);
+const options = parse_parameter(process.argv.slice(2));
 compile(options);
