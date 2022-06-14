a0cb44ca0a4db83b6ec58b72aa85880bd9a9fb00
ramenha0141 <ramenhaoishii@gmail.com>
2022-06-14 19:44:21 +0900

    update

--- src/builtins/console.ts
+++ src/builtins/console.ts
@@ -9,10 +9,11 @@ const console = (llvmModule: llvm.Module) => {
     const printf_type = llvm.FunctionType.get(i32, [i8_ptr], true);
     const printf = llvm.Function.Create(printf_type, 'printf', llvmModule);
     // put int
+    const puts_str = builder.CreateGlobalString('%s\n', 'puts_str');
     const puts_type = llvm.FunctionType.get(i32, [i8_ptr], false);
     const puts = llvm.Function.Create(puts_type, '"console::puts"', llvmModule);
     builder.SetInsertPoint(llvm.BasicBlock.Create(puts));
-    builder.CreateRet(builder.CreateCall(printf, [puts.getArg(0)]));
+    builder.CreateRet(builder.CreateCall(printf, [puts_str, puts.getArg(0)]));
     console.import('puts', puts);
     return console;
 };
